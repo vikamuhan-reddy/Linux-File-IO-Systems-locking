d1cb1317bfd2f478339f440cfcbe3f8dd4824a01
vikamuhan-reddy <144928933+vikamuhan-reddy@users.noreply.github.com>
2024-05-07 14:09:35 +0530

    success

--- filecopy.c
+++ filecopy.c
@@ -1,1 +1,13 @@
-/
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+int main()
+{
+char block[1024];
+int in, out;
+int nread;
+in = open("filecopy.c", O_RDONLY);
+out = open("file.out", O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+while((nread = read(in,block,sizeof(block))) > 0)
+write(out,block,nread);
+exit(0);}/
